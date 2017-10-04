777fbd2ccad1123cef35d3a22fd02af332194dde
Tim Lachmann <Tim@Lachmann.nu>
2017-10-04 17:00:48 +0200

    route

---
+++ src/Route.java
@@ -0,0 +1,9 @@
+public class Route {
+    public double radius;
+    public Point2D position;
+
+    public Route(double radius, Point2D position) {
+        this.radius = radius;
+        this.position = position;
+    }
+}
