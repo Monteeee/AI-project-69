fe3770a2189c9bb3fec85b8203babedea276d877
Monteeee <montemiracle@live.com>
2017-10-11 08:56:22 +0200

    fix small bugs in planning and view

--- src/Constants.java
+++ src/Constants.java
@@ -7,11 +7,11 @@ public class Constants {
     public static final double AGENT_START_POS_X = 450d;
     public static final double AGENT_START_POS_Y = 450d;
     public static final int NUM_OF_ROVERS = 4;
-    public static final int NUM_OF_OBSTACLES = 6;
+    public static final int NUM_OF_OBSTACLES = 3;
     public static final int INIT_SCORE = 1000;
     public static final int SCORE_PER_ROVER = 100;
-    public static final double ROVER_INIT_SPEED = 0.5;
-    public static final double ROVER_INIT_ANGULAR_VELOCITY = 0.03;
+    public static final double ROVER_INIT_SPEED = 1.2;
+    public static final double ROVER_INIT_ANGULAR_VELOCITY = 0.04;
     public static final double AGENT_INIT_SPEED = 1;
     public static final double AGENT_INIT_ANGULAR_VELOCITY = 0;
 }
